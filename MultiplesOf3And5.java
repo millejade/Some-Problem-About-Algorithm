a060b8fada94db5a4897e71524ad58a5ef9f01d7
Mille Jade Cabrera <36434363+millejade@users.noreply.github.com>
2021-11-18 21:51:48 +0800

    Create MultiplesOf3And5.java

---
+++ MultiplesOf3And5.java
@@ -0,0 +1,21 @@
+public class MultiplesOf3And5 {
+
+    public static void main(String[] args) {
+       
+        int sum = 0; 
+        
+        for(int i = 3; i < 1000; i++){
+        
+            if(i % 3 == 0){
+                
+                sum += i;
+            }else if(i > 4 && i % 5 == 0){
+                sum += i;
+            }
+        
+        }
+        System.out.println(sum);
+        
+    }
+    
+}
